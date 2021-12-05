afed48f269c0a671532cd5f60bb6cea2094ecb6c
davidprager <david_prager@yahoo.com>
2021-12-04 20:17:06 -0500

    Initial commit

---
+++ conditions.js
@@ -0,0 +1,84 @@
+/* This file will be used to test some basc JavaScript conditions and loops */
+/* It assumes that the only valid temperatures are 30, 40, 50, 60, 70, 80 and 90
+*/
+
+"use strict"
+
+
+/* This function uses a simple if statement
+   to check if the temperature is 70 degrees
+ */
+/*function tempCheck() {
+    let temp = 70;
+    console.log("the temperature is " + temp+ " degrees" );
+
+    }
+}*/
+
+
+/* This function uses an if statement with multiple conditions
+   to check if the temperature is 70 or 80 degrees
+*/
+// function tempCheck() {
+//     let temp = 60;
+//     console.log("the temperature is " + temp+ " degrees" );
+
+//     }
+// }
+
+
+/* This function uses an if statement with multiple conditions
+   to check if the temperature is 70 or 80 degrees
+   based upon an input argument. The message is different depending upon the result.
+*/
+
+/*function tempCheck() {
+
+}*/
+
+/* This function uses an if statement with multiple conditions
+   to check if the temperature is 70 or 80 degrees
+   based upon an input argument. The input is validated before processing.
+   The message is then different depending upon the result.
+*/
+
+/*function tempCheck() {
+
+}*/
+
+function validateInput(temp) {
+
+}
+
+
+/* This function uses multiple if/else statements
+   to check the temperature based upon an input argument
+    and will display a relevant message, assuming the
+    temperature is valid
+*/
+/*function tempCheck(temp) {
+
+}*/
+
+/* This function uses a switch statement
+   to check the temperature based upon an input argument
+    and will display a relevant message, assuming the
+    temperature is valid
+*/
+/*function tempCheck() {
+
+}*/
+
+/* This function uses a while loop
+   to display temperatures and descriptions
+*/
+/*function tempDisplay() {
+
+}*/
+
+/* This function uses a for loop
+   to display temperatures and descriptions
+*/
+/*function tempDisplay() {
+
+}*/
